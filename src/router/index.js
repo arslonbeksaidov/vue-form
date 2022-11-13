fc622be85630002431dd54915dd38a5b17229de6
arslon <a4317990a>
2022-11-13 11:53:34 +0500

    route props improvement

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import PageThreadShow from '@/components/PageThreadShow'
 import PageHome from '@/components/PageHome'
 import PageNotFound from '@/components/PageNotFound'
 import { createRouter, createWebHistory } from 'vue-router'
+import dataJson from '@/data.json'
 
 const routes = [
   {
@@ -12,8 +13,21 @@ const routes = [
   {
     path: '/thread/:id',
     name: 'ThreadShow',
-    props: true,
-    component: PageThreadShow
+    props: route => ({ id: route.params.id }),
+    component: PageThreadShow,
+    beforeEnter (to, from, next) {
+      const threadExists = dataJson.threads.find(thread => thread.id === to.params.id)
+      if (threadExists) {
+        return next()
+      } else {
+        next({
+          name: 'NotFound',
+          params: { pathMatch: to.path.substring(1).split('/') },
+          query: to.query,
+          hash: to.hash
+        })
+      }
+    }
   },
   {
     path: '/:pathMatch(.*)*',
